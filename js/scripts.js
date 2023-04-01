e871abde426fee11e5a1891c4b8b58ec15c44e7a
Iasmim Rodrigues <iasmimrodrigues1301@gmail.com>
2023-04-01 16:17:54 -0300

    Média Final - G2

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,9 @@ const retornarMenor = function(vetor){
 
 }
 const retornarMedia = function(vetor){
-
+    let total = vetor.reduce((soma, vetor) => soma + vetor, 0);
+    const media = total / vetor.length;
+    return media;
 }
 const retornarOrdenarCrescente = function(vetor){
 
