a6a67c1e4c1b6325516f24a82281224367e150b9
Cristiano <cristiano.martins01@fatec.sp.gov.br>
2023-04-01 12:44:56 -0300

    testando

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
 const retornarMaior = function(vetor){
 
-
 }
 const retornarMenor = function(vetor){
-
 
 }
 const retornarMedia = function(vetor){
 
-
 }
 const retornarOrdenarCrescente = function(vetor){
-
 
 }
 const retornarOrdenarDecrescente = function(vetor){
 
-    
 }
